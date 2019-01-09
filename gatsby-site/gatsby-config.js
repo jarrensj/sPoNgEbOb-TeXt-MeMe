47f67b8241efcfa9194669a33482f3f64b7bbd00
Jarren San Jose <sanjose.jarren@gmail.com>
2019-01-08 22:40:43 -0800

    fOrM tO tRaNsFoRm InPuT

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `sPoNgEbOb TeXt MeMe`,
+    description: `An app to convert text to alternate between lowercase and uppercase letters.`,
     author: `@gatsbyjs`,
   },
   plugins: [
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -5,17 +5,75 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default class IndexPage extends React.Component {
+  state = {
+    input: "",
+    output: ""
+  }
 
-export default IndexPage
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    var spongebobText = spongebob(this.state.input);
+    this.setState({
+      output: spongebobText
+    })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+        <h1>eNtEr TeXt BeLoW</h1>
+        <form onSubmit={this.handleSubmit}>
+        <input
+            type="text"
+            name="input"
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">suBmIt</button>
+       </form>
+       { this.state.output ? <h2>yOuR tRaNsFoRmEd TeXt</h2> : null }
+       {this.state.output}
+       <br />
+       <br />
+      </Layout>
+    )
+  }
+
+}
+
+const isLetter = char => {
+  return char.toLowerCase() != char.toUpperCase();
+}
+
+const spongebob = text => {
+  let newText = '';
+  let uppercase = false;
+  for(let i = 0; i < text.length; ++i) {
+    if(isLetter(text.charAt(i))) {
+      if(!uppercase) {
+        newText += text.charAt(i).toLowerCase();
+        uppercase = true;
+      }
+      else {
+        newText += text.charAt(i).toUpperCase();
+        uppercase = false;
+      }
+    }
+    else {
+      newText += text.charAt(i);
+    }
+  }
+  return newText;
+}
